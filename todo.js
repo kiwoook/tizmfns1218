0dfb369802d62bfc817f46074efbc3621ade74ad
kiwoook <72564777+kiwoook@users.noreply.github.com>
2021-09-03 23:46:52 +0900

    version2

---
+++ todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+let toDos = [];
+
+const TODOS_KEY = "todos";
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+    
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "✓"
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+  }
+  function handleToDoSubmit(event) {
+    event.preventDefault();
+    let obj_length = Object.keys(toDos).length;
+    if(obj_length > 12)
+    {
+      alert('공간이 부족합니다 :(');
+      return
+    }
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+      text: newTodo,
+      id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+  }
+  toDoForm.addEventListener("submit", handleToDoSubmit);
+  const savedToDos = localStorage.getItem(TODOS_KEY);
+  if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+  }
