6dcb502c3e22d61f085120f9d32acda49433a9d1
kiwoook <72564777+kiwoook@users.noreply.github.com>
2021-09-04 20:15:09 +0900

    version4

--- js/greetings.js
+++ js/greetings.js
@@ -7,7 +7,7 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
+    greeting.innerText = `반갑습니다 ${username}님 :)`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
